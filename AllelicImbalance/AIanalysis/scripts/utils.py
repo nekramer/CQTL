950e98fa4b4c7444f38f1b6445f291cffd386ffa
Nicole Kramer <nekramer@live.unc.edu>
2022-08-24 15:46:58 -0400

    edited getRsids for different dbSNP paths and use in converted Boer LD buddies from pos to rsid

--- AllelicImbalance/AIanalysis/scripts/utils.py
+++ AllelicImbalance/AIanalysis/scripts/utils.py
@@ -2,13 +2,11 @@
 import allel
 import pandas as pd
 # Function to convert variant positions to variant rsids based on a dbSNP reference vcf
-def getRsids(variantPositions):
+def getRsids(variantPositions, dbSNP = '/proj/phanstiel_lab/References/genomes/GENCODE.GRCh38.p13/dbSNP/dbSNP155.GRCh38.p13.vcf.gz'):
     
     def perVariantRSID(variant):
-
         regionString = str(variant[0]) + ':' + str(variant[1]) + '-' + str(variant[1])
-        rsid = allel.read_vcf('/proj/phanstiel_lab/References/genomes/GENCODE.GRCh38.p13/dbSNP/dbSNP155.GRCh38.p13.vcf.gz', 
-                        region = regionString, fields = ['ID'])
+        rsid = allel.read_vcf(dbSNP, region = regionString, fields = ['ID'])
         return(rsid['variants/ID'])
         
     # Make sure input is DataFrame
