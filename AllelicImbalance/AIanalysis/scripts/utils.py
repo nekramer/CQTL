e08f2ac5b09a7523e05d2c2cc2777ffd1e971589
Nicole Kramer <nekramer@live.unc.edu>
2022-08-25 10:30:13 -0400

    parallelize posToRsid and catch statement for when location not found

--- AllelicImbalance/AIanalysis/scripts/utils.py
+++ AllelicImbalance/AIanalysis/scripts/utils.py
@@ -1,17 +1,27 @@
 # utils.py>
 import allel
 import pandas as pd
+from pandarallel import pandarallel
+
 # Function to convert variant positions to variant rsids based on a dbSNP reference vcf
 def getRsids(variantPositions, dbSNP = '/proj/phanstiel_lab/References/genomes/GENCODE.GRCh38.p13/dbSNP/dbSNP155.GRCh38.p13.vcf.gz'):
     
     def perVariantRSID(variant):
         regionString = str(variant[0]) + ':' + str(variant[1]) + '-' + str(variant[1])
         rsid = allel.read_vcf(dbSNP, region = regionString, fields = ['ID'])
-        return(rsid['variants/ID'])
+        
+        # Catch if rsid position not found
+        if rsid is None:
+            return(str(variant[0]) + ':' + str(variant[1]))
+        else:
+            return(rsid['variants/ID'])
         
     # Make sure input is DataFrame
     variantPositions = pd.DataFrame(variantPositions)
+    
+    # Initialize pandarallel
+    pandarallel.initialize(progress_bar = False)
 
-    # Apply perVariantRSID to every row
-    variantRSIDs = variantPositions.apply(perVariantRSID, axis = 1)
+    # Apply perVariantRSID to every row, using pandarallel apply
+    variantRSIDs = variantPositions.parallel_apply(perVariantRSID, axis = 1)
     return(variantRSIDs)
